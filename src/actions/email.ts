e739ef6eee58b59aa29d492a2674ad2010e87f5b
Pranjal Jain <pranjalworkon@gmail.com>
2025-07-31 01:45:39 +0530

    fix: email issue reverted

--- src/actions/email.ts
+++ src/actions/email.ts
@@ -15,9 +15,9 @@ export async function send(formData: FormData) {
     }
 
     try {
-        await resend.emails.send({
-            // need to change the domain to send from the user email only
-            from: email,
+            await resend.emails.send({
+            // need to change the domain to send from the user email only 
+            from: 'onboarding@resend.dev',
             to: ['pranjalworkon@gmail.com'],
             subject: 'Hey you got a new query from your portfolio',
             react: EmailTemplate({ name, email, message }),
