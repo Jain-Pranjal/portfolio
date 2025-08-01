ab4f7dc4ad898b9c95e08fa02c7a6c9ca8f537e2
Pranjal Jain <pranjalworkon@gmail.com>
2025-08-01 11:36:10 +0530

    fix: middleware routing config

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,5 +15,8 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  matcher: [
+    '/((?!_next/|favicon.ico|images/|icons/|public/).*)',
+  ],
+
 }
